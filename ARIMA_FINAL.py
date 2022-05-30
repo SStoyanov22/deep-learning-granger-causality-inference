c03c721deeef89a8d9944055e7bc303f2544f59e
Gitea <SStoyanov22@gmail.com>
2022-05-30 08:20:40 +0300

    Adding final version for ARIMA + LSTM for btc only

--- ARIMA_FINAL.py
+++ ARIMA_FINAL.py
@@ -1,23 +1,26 @@
 #import libraties
-
-from datetime import datetime
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import glob 
 from statsmodels import api as sm
-from scipy.stats import linregress
-import matplotlib.pyplot as plt
 #conda install yahoofinancials
 from yahoofinancials import YahooFinancials
 
+#### import libraries for ARIMA
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf
+#pip install pmdarima
+from pmdarima import auto_arima
+# Ignore harmless warnings
+import warnings
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima.model import ARIMA
 
 """Constants"""
 TICKER_BTC = "BTC-USD"
 TICKER_NASDAQ = "^IXIC"
 TICKER_SNP = "^GSPC"
 START_DATE = "2014-09-17"
-END_DATE = "2022-04-07"
+END_DATE = "2022-03-31"
 pd.set_option('display.max_columns', 7)
 pd.set_option('display.max_rows', 100)
 
@@ -94,22 +97,18 @@ df.fillna(method='ffill', inplace=True)
 ###############################ARIMA MODEL###############################
 
 #check data for stationarity
-from statsmodels.tsa.stattools import adfuller
 plt.plot('BTC',data=df)
-adfuller(df["BTC"]) # p value 0.76
-
+plt.title("Price BTC")
+adfuller(df["BTC"]) # p value 0.79
 #value is not stationary and has to be normalized
 
-
-from statsmodels.graphics.tsaplots import plot_acf
-
 #1st order differencing
-diff1_plot = plt.figure().add_subplot()
-diff1_plot.set_title('1st Order Differencing')
-diff1_plot.plot(df['BTC'].diff())
+plt.plot(df['BTC'].diff())
+plt.title("1st Order Differencing")
 
+#performing ADF test to check if the data after 1st differencing is stationary
 adfuller(df['BTC'].diff().dropna())
-#p value < 0.05 therefore data is normalized after 1 period
+#p value is < 0.05 therefore data is normalized after 1 period
 
 #acf after 1 differencing
 plot_acf(df['BTC'].diff().dropna())
@@ -117,46 +116,41 @@ plot_acf(df['BTC'].diff().dropna())
 #pacf after 1 differencing
 sm.graphics.tsa.plot_pacf(df["BTC"].diff().dropna())
 
+# Create Training and Test data sets; 75% of the data is for training purposes
+train = btc_df.BTC[:2202]
+test = btc_df.BTC[2202:]
 
-#pip install pmdarima
-from pmdarima import auto_arima
-# Ignore harmless warnings
-import warnings
+# Auto Arima for best possible combination of p,d,q
 warnings.filterwarnings("ignore")
-#Calling our model and generating best possible ARIMA combination
- #calling our function
-auto_model1 = auto_arima(df["BTC"],suppress_warnings=True)           
-auto_model1.summary() # model to be 0,1,0
-
-# Create Training and Test
-train = df.BTC[:2070]
-test = df.BTC[2070:]
+auto_model = auto_arima(train,suppress_warnings=True)           
+auto_model.summary() # model to be 0,1,0
 
 # Build Model
 
-from statsmodels.tsa.arima_model import ARIMA
-import warnings
-warnings.filterwarnings('ignore', 'statsmodels.tsa.arima_model.ARMA',
-                        FutureWarning)
-warnings.filterwarnings('ignore', 'statsmodels.tsa.arima_model.ARIMA',
-                        FutureWarning)
- 
-model = ARIMA(train, order=(0, 1, 0))  
-fitted = model.fit(disp=-1)  
+#checking data for seasonality
+result=seasonal_decompose(btc_df['BTC'], model='multiplicable', period=365)
+result.seasonal.plot()
+result.trend.plot()
+result.plot()
+#after decomposing the data, it looks seasonal
+
+#showing our model that there is seasonality
+model = ARIMA(train, order=(0, 1, 0), seasonal_order=(0,1,0,365))
+res=model.fit()
+print(res.summary())
 
 # Forecast
-fc, se, conf = fitted.forecast(690, alpha=0.05)  # 95% conf
+forecast = res.forecast(551,alpha=0.05)
 
-# Make as pandas series
-fc_series = pd.Series(fc, index=test.index)
-lower_series = pd.Series(conf[:, 0], index=test.index)
-upper_series = pd.Series(conf[:, 1], index=test.index)
+forecast_series = pd.Series(forecast, index=test.index)
+lower_series = pd.Series(index=test.index)
+upper_series = pd.Series(index=test.index)
 
 # Plot
 plt.figure(figsize=(12,5), dpi=100)
 plt.plot(train, label='training')
 plt.plot(test, label='actual')
-plt.plot(fc_series, label='forecast')
+plt.plot(forecast_series, label='forecast')
 plt.fill_between(lower_series.index, lower_series, upper_series, 
                  color='k', alpha=.15)
 plt.title('Forecast vs Actuals')
